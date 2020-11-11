3220fce3da938c2210a5e42aa8d177a17e04bce5
geus-junior <geusjunior@gipisistemas.com.br>
2020-11-11 18:03:01 -0300

    3 files modified

--- src/app/form-field-appearance-example.ts
+++ src/app/form-field-appearance-example.ts
@@ -1,16 +1,21 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
 /** @title Form field appearance variants */
 @Component({
-  selector: 'form-field-appearance-example',
-  templateUrl: 'form-field-appearance-example.html',
-  styleUrls: ['form-field-appearance-example.css'],
-  encapsulation: ViewEncapsulation.None,
+  selector: "form-field-appearance-example",
+  templateUrl: "form-field-appearance-example.html",
+  styleUrls: ["form-field-appearance-example.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class FormFieldAppearanceExample {
 
-  filterValue(value: string): void {
-    
-  }
+  public releaseCode: string = "1231126421654154154656485649659859745654548765955454";
+  public serialNumber: string = ""  ;
 
+  onClick(id: string): void {
+
+    let lElement = document.getElementById(id);
+    lElement.select();
+
+  }
 }
